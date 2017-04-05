f451c261fbe3b5d38b7383f14af2acc92882294a
Jose Torres <jrtg94@users.noreply.github.com>
2017-04-05 11:01:50 -0400

    Dice Simulator
    Visual Studio Project

---
+++ Dice Simulator/Dice Simulator/Form1.Designer.vb
@@ -0,0 +1,268 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDice = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.pic2DiceSide6 = New System.Windows.Forms.PictureBox()
+        Me.pic2DiceSide5 = New System.Windows.Forms.PictureBox()
+        Me.pic2DiceSide4 = New System.Windows.Forms.PictureBox()
+        Me.pic2DiceSide3 = New System.Windows.Forms.PictureBox()
+        Me.pic2DiceSide2 = New System.Windows.Forms.PictureBox()
+        Me.pic2DiceSide1 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.pic1DiceSide6 = New System.Windows.Forms.PictureBox()
+        Me.pic1DiceSide5 = New System.Windows.Forms.PictureBox()
+        Me.pic1DiceSide4 = New System.Windows.Forms.PictureBox()
+        Me.pic1DiceSide3 = New System.Windows.Forms.PictureBox()
+        Me.pic1DiceSide2 = New System.Windows.Forms.PictureBox()
+        Me.pic1DiceSide1 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.pic2DiceSide6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic2DiceSide5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic2DiceSide4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic2DiceSide3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic2DiceSide2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic2DiceSide1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.pic1DiceSide6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic1DiceSide5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic1DiceSide4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic1DiceSide3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic1DiceSide2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic1DiceSide1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnDice
+        '
+        Me.btnDice.Location = New System.Drawing.Point(179, 321)
+        Me.btnDice.Name = "btnDice"
+        Me.btnDice.Size = New System.Drawing.Size(100, 41)
+        Me.btnDice.TabIndex = 0
+        Me.btnDice.Text = "Roll Dice"
+        Me.btnDice.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(478, 321)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(95, 41)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.pic2DiceSide6)
+        Me.GroupBox1.Controls.Add(Me.pic2DiceSide5)
+        Me.GroupBox1.Controls.Add(Me.pic2DiceSide4)
+        Me.GroupBox1.Controls.Add(Me.pic2DiceSide3)
+        Me.GroupBox1.Controls.Add(Me.pic2DiceSide2)
+        Me.GroupBox1.Controls.Add(Me.pic2DiceSide1)
+        Me.GroupBox1.Location = New System.Drawing.Point(367, 32)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(358, 240)
+        Me.GroupBox1.TabIndex = 14
+        Me.GroupBox1.TabStop = False
+        '
+        'pic2DiceSide6
+        '
+        Me.pic2DiceSide6.Image = Global.Dice_Simulator.My.Resources.Resources.Die6
+        Me.pic2DiceSide6.Location = New System.Drawing.Point(33, 19)
+        Me.pic2DiceSide6.Name = "pic2DiceSide6"
+        Me.pic2DiceSide6.Size = New System.Drawing.Size(270, 194)
+        Me.pic2DiceSide6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pic2DiceSide6.TabIndex = 5
+        Me.pic2DiceSide6.TabStop = False
+        '
+        'pic2DiceSide5
+        '
+        Me.pic2DiceSide5.Image = Global.Dice_Simulator.My.Resources.Resources.Die5
+        Me.pic2DiceSide5.Location = New System.Drawing.Point(33, 19)
+        Me.pic2DiceSide5.Name = "pic2DiceSide5"
+        Me.pic2DiceSide5.Size = New System.Drawing.Size(270, 194)
+        Me.pic2DiceSide5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pic2DiceSide5.TabIndex = 4
+        Me.pic2DiceSide5.TabStop = False
+        '
+        'pic2DiceSide4
+        '
+        Me.pic2DiceSide4.Image = Global.Dice_Simulator.My.Resources.Resources.Die4
+        Me.pic2DiceSide4.Location = New System.Drawing.Point(33, 19)
+        Me.pic2DiceSide4.Name = "pic2DiceSide4"
+        Me.pic2DiceSide4.Size = New System.Drawing.Size(270, 194)
+        Me.pic2DiceSide4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pic2DiceSide4.TabIndex = 3
+        Me.pic2DiceSide4.TabStop = False
+        '
+        'pic2DiceSide3
+        '
+        Me.pic2DiceSide3.Image = Global.Dice_Simulator.My.Resources.Resources.Die3
+        Me.pic2DiceSide3.Location = New System.Drawing.Point(33, 19)
+        Me.pic2DiceSide3.Name = "pic2DiceSide3"
+        Me.pic2DiceSide3.Size = New System.Drawing.Size(270, 194)
+        Me.pic2DiceSide3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pic2DiceSide3.TabIndex = 2
+        Me.pic2DiceSide3.TabStop = False
+        '
+        'pic2DiceSide2
+        '
+        Me.pic2DiceSide2.Image = Global.Dice_Simulator.My.Resources.Resources.Die2
+        Me.pic2DiceSide2.Location = New System.Drawing.Point(33, 19)
+        Me.pic2DiceSide2.Name = "pic2DiceSide2"
+        Me.pic2DiceSide2.Size = New System.Drawing.Size(270, 194)
+        Me.pic2DiceSide2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pic2DiceSide2.TabIndex = 1
+        Me.pic2DiceSide2.TabStop = False
+        '
+        'pic2DiceSide1
+        '
+        Me.pic2DiceSide1.Image = Global.Dice_Simulator.My.Resources.Resources.Die11
+        Me.pic2DiceSide1.Location = New System.Drawing.Point(33, 19)
+        Me.pic2DiceSide1.Name = "pic2DiceSide1"
+        Me.pic2DiceSide1.Size = New System.Drawing.Size(270, 194)
+        Me.pic2DiceSide1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pic2DiceSide1.TabIndex = 0
+        Me.pic2DiceSide1.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.pic1DiceSide6)
+        Me.GroupBox2.Controls.Add(Me.pic1DiceSide5)
+        Me.GroupBox2.Controls.Add(Me.pic1DiceSide4)
+        Me.GroupBox2.Controls.Add(Me.pic1DiceSide3)
+        Me.GroupBox2.Controls.Add(Me.pic1DiceSide2)
+        Me.GroupBox2.Controls.Add(Me.pic1DiceSide1)
+        Me.GroupBox2.Location = New System.Drawing.Point(24, 32)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(337, 240)
+        Me.GroupBox2.TabIndex = 15
+        Me.GroupBox2.TabStop = False
+        '
+        'pic1DiceSide6
+        '
+        Me.pic1DiceSide6.Image = Global.Dice_Simulator.My.Resources.Resources.Die6
+        Me.pic1DiceSide6.Location = New System.Drawing.Point(30, 19)
+        Me.pic1DiceSide6.Name = "pic1DiceSide6"
+        Me.pic1DiceSide6.Size = New System.Drawing.Size(265, 194)
+        Me.pic1DiceSide6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pic1DiceSide6.TabIndex = 5
+        Me.pic1DiceSide6.TabStop = False
+        '
+        'pic1DiceSide5
+        '
+        Me.pic1DiceSide5.Image = Global.Dice_Simulator.My.Resources.Resources.Die5
+        Me.pic1DiceSide5.Location = New System.Drawing.Point(30, 19)
+        Me.pic1DiceSide5.Name = "pic1DiceSide5"
+        Me.pic1DiceSide5.Size = New System.Drawing.Size(265, 194)
+        Me.pic1DiceSide5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pic1DiceSide5.TabIndex = 4
+        Me.pic1DiceSide5.TabStop = False
+        '
+        'pic1DiceSide4
+        '
+        Me.pic1DiceSide4.Image = Global.Dice_Simulator.My.Resources.Resources.Die4
+        Me.pic1DiceSide4.Location = New System.Drawing.Point(30, 19)
+        Me.pic1DiceSide4.Name = "pic1DiceSide4"
+        Me.pic1DiceSide4.Size = New System.Drawing.Size(265, 194)
+        Me.pic1DiceSide4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pic1DiceSide4.TabIndex = 3
+        Me.pic1DiceSide4.TabStop = False
+        '
+        'pic1DiceSide3
+        '
+        Me.pic1DiceSide3.Image = Global.Dice_Simulator.My.Resources.Resources.Die3
+        Me.pic1DiceSide3.Location = New System.Drawing.Point(30, 19)
+        Me.pic1DiceSide3.Name = "pic1DiceSide3"
+        Me.pic1DiceSide3.Size = New System.Drawing.Size(265, 194)
+        Me.pic1DiceSide3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pic1DiceSide3.TabIndex = 2
+        Me.pic1DiceSide3.TabStop = False
+        '
+        'pic1DiceSide2
+        '
+        Me.pic1DiceSide2.Image = Global.Dice_Simulator.My.Resources.Resources.Die2
+        Me.pic1DiceSide2.Location = New System.Drawing.Point(30, 19)
+        Me.pic1DiceSide2.Name = "pic1DiceSide2"
+        Me.pic1DiceSide2.Size = New System.Drawing.Size(265, 194)
+        Me.pic1DiceSide2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pic1DiceSide2.TabIndex = 1
+        Me.pic1DiceSide2.TabStop = False
+        '
+        'pic1DiceSide1
+        '
+        Me.pic1DiceSide1.Image = Global.Dice_Simulator.My.Resources.Resources.Die1
+        Me.pic1DiceSide1.Location = New System.Drawing.Point(30, 19)
+        Me.pic1DiceSide1.Name = "pic1DiceSide1"
+        Me.pic1DiceSide1.Size = New System.Drawing.Size(265, 194)
+        Me.pic1DiceSide1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pic1DiceSide1.TabIndex = 0
+        Me.pic1DiceSide1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(759, 391)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnDice)
+        Me.Name = "Form1"
+        Me.Text = "Dice Simulator"
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.pic2DiceSide6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic2DiceSide5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic2DiceSide4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic2DiceSide3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic2DiceSide2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic2DiceSide1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        CType(Me.pic1DiceSide6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic1DiceSide5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic1DiceSide4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic1DiceSide3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic1DiceSide2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic1DiceSide1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnDice As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents pic2DiceSide6 As PictureBox
+    Friend WithEvents pic2DiceSide5 As PictureBox
+    Friend WithEvents pic2DiceSide4 As PictureBox
+    Friend WithEvents pic2DiceSide3 As PictureBox
+    Friend WithEvents pic2DiceSide2 As PictureBox
+    Friend WithEvents pic2DiceSide1 As PictureBox
+    Friend WithEvents pic1DiceSide6 As PictureBox
+    Friend WithEvents pic1DiceSide5 As PictureBox
+    Friend WithEvents pic1DiceSide4 As PictureBox
+    Friend WithEvents pic1DiceSide3 As PictureBox
+    Friend WithEvents pic1DiceSide2 As PictureBox
+    Friend WithEvents pic1DiceSide1 As PictureBox
+End Class
---
+++ Dice Simulator/Dice Simulator/Form1.vb
@@ -0,0 +1,75 @@
+﻿Public Class Form1
+    Private Sub btnDice_Click(sender As Object, e As EventArgs) Handles btnDice.Click
+
+        Dim Dice1 As Integer 'Indicate first dice roll
+        Dim Dice2 As Integer 'Indicate second dice roll
+        Dim ran As New Random 'Roll the dices randomly
+
+        pic1DiceSide1.Visible = False
+        pic1DiceSide2.Visible = False
+        pic1DiceSide3.Visible = False
+        pic1DiceSide4.Visible = False
+        pic1DiceSide5.Visible = False
+        pic1DiceSide6.Visible = False
+        pic2DiceSide1.Visible = False
+        pic2DiceSide2.Visible = False
+        pic2DiceSide3.Visible = False
+        pic2DiceSide4.Visible = False
+        pic2DiceSide5.Visible = False
+        pic2DiceSide6.Visible = False
+
+        Dice1 = ran.Next(1, 6)
+        Dice2 = ran.Next(1, 6)
+
+        Select Case Dice1
+
+            Case 1
+                pic1DiceSide1.Visible = True
+
+            Case 2
+                pic1DiceSide2.Visible = True
+
+            Case 3
+                pic1DiceSide3.Visible = True
+
+            Case 4
+                pic1DiceSide4.Visible = True
+
+            Case 5
+                pic1DiceSide5.Visible = True
+
+            Case 6
+                pic1DiceSide6.Visible = True
+
+        End Select
+
+        Select Case Dice2
+
+            Case 1
+                pic2DiceSide1.Visible = True
+
+            Case 2
+                pic2DiceSide2.Visible = True
+
+            Case 3
+                pic2DiceSide3.Visible = True
+
+            Case 4
+                pic2DiceSide4.Visible = True
+
+            Case 5
+                pic2DiceSide5.Visible = True
+
+            Case 6
+                pic2DiceSide6.Visible = True
+
+        End Select
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        Me.Close()
+
+    End Sub
+End Class
